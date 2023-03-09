ffe861034c53e65fc289ef0834ba4d9889390bac
Swayam088 <126493897+Swayam088@users.noreply.github.com>
2023-03-09 14:00:12 +0530

    Add files via upload

---
+++ qtn2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  char num[9];
+  scanf("%s",&num);
+  if(num[2]!='A'&&num[2]!='E'&&num[2]!='I'&&num[2]!='O'&& num[2]!='U'&& num[2]!='Y' &&(num[0]+num[1])%2==0 && (num[3]+num[4])%2==0 && (num[5]+num[6])%2==0 && (num[8]+num[9])%2==0){
+    printf("valid");
+  }
+
+  else{
+    printf("invalid");
+  }
+  return 0;
+
+
+}
